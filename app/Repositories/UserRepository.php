6726f7d479720efd66ca778013f51f5eeeee20b3
khaalid <khaalid@io.co.za>
2023-03-28 22:40:46 +0200

    implemented interface

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\UserRepositoryInterface;
+
+class UserRepository extends BaseRepository implements UserRepositoryInterface
+{
+
+    public function createUser(array $userDetails)
+    {
+        // TODO: Implement createUser() method.
+    }
+
+    public function updateUser(array $newDetails)
+    {
+        // TODO: Implement updateUser() method.
+    }
+}
