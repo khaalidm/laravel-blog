0c0a38be86ce79b8a24818a05b43ee1af8753566
khaalid <khaalid@io.co.za>
2023-03-28 22:40:29 +0200

    implemented interface

---
+++ app/Repositories/CommentRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\CommentRepositoryInterface;
+
+class CommentRepository extends BaseRepository implements CommentRepositoryInterface
+{
+
+    public function createComment(array $commentDetails)
+    {
+        // TODO: Implement createComment() method.
+    }
+
+    public function updateComment(array $newDetails)
+    {
+        // TODO: Implement updateComment() method.
+    }
+}
