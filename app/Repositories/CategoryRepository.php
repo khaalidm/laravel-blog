4ad1590b0cf5cadbd6e45546d7a70e8136eb8979
khaalid <khaalid@io.co.za>
2023-03-28 22:40:17 +0200

    implemented interface

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\CategoryRepositoryInterface;
+
+class CategoryRepository extends BaseRepository implements CategoryRepositoryInterface
+{
+
+    public function createCategory(array $categoryDetails)
+    {
+        // TODO: Implement createCategory() method.
+    }
+
+    public function updateCategory(array $newDetails)
+    {
+        // TODO: Implement updateCategory() method.
+    }
+}
