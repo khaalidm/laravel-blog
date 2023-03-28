39c3af56252b37cfef89f7d20024bb57f125e0b5
khaalid <khaalid@io.co.za>
2023-03-28 22:40:38 +0200

    implemented interface

---
+++ app/Repositories/PostRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\PostRepositoryInterface;
+
+class PostRepository extends BaseRepository implements PostRepositoryInterface
+{
+
+    public function createPost(array $postDetails)
+    {
+        // TODO: Implement createPost() method.
+    }
+
+    public function updatePost(array $newDetails)
+    {
+        // TODO: Implement updatePost() method.
+    }
+}
