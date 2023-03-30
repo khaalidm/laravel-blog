2ef6ae3bc41e35c3db8f04653243b4357631eede
khaalid <khaalid@io.co.za>
2023-03-30 16:15:41 +0200

    Added post factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $paragraphs = $this->faker->paragraphs(rand(2, 6));
+        foreach ($paragraphs as $para) {
+            $body .= "<p>{$para}</p>";
+        }
+
+        return [
+            'user_id'       => User::factory(),
+            'category_id'   => Category::factory(),
+            'title'         => $this->faker->realText(50),
+            'description'   => $this->faker->realText(120),
+            'body'          => $body,
+            'active'        => true,
+            'archived_at'   => null,
+            'created_at'    => Carbon::now(),
+            'updated_at'    => Carbon::now()
+
+        ];
+    }
+}
