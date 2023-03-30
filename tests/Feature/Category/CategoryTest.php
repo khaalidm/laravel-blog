e9813fb0adc2a08c3ad852cc53c3433de0e3224f
khaalid <khaalid@io.co.za>
2023-03-30 16:14:25 +0200

    Added test to check if roles are seeded

--- tests/Feature/Category/CategoryTest.php
+++ tests/Feature/Category/CategoryTest.php
@@ -2,9 +2,17 @@
 
 namespace Tests\Feature\Category;
 
+use App\Models\Role;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
+    public function testRolesAreSeeded(): void
+    {
+        self::assertTrue(Role::where('name', Role::SUPER)->first() instanceof Role);
+        self::assertTrue(Role::where('name', Role::ADMIN)->first() instanceof Role);
+        self::assertTrue(Role::where('name', Role::AUTHOR)->first() instanceof Role);
+        self::assertTrue(Role::where('name', Role::BASIC_USER)->first() instanceof Role);
+    }
 
 }
