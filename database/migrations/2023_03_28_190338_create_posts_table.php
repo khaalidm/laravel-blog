4553ad6219041ab1d6d37fb6698306a61315f30b
khaalid <khaalid@io.co.za>
2023-03-30 21:29:58 +0200

    Removed the archived_at field

--- database/migrations/2023_03_28_190338_create_posts_table.php
+++ database/migrations/2023_03_28_190338_create_posts_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->string('description');
             $table->text('body');
             $table->boolean('active')->default(true);
-            $table->dateTime('archived_at')->nullable();
             $table->timestamps();
         });
     }
