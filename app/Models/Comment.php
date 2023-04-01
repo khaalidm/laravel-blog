efe9b07ecf92d466af117f7023f3ee303d44dbbc
khaalid <khaalid@io.co.za>
2023-04-01 18:40:33 +0200

    Added relationships in comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,18 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     use HasFactory;
 
+    //-------------------------RELATIONSHIPS------------------------//
+
     protected $fillable = [
         'user_id',
-        'category_id',
-        'title',
-        'description',
-        'body',
+        'post_id',
+        'text',
+        'created_at',
+        'updated_at'
     ];
 
-    //-------------------------RELATIONSHIPS------------------------//
+    /**
+     * A comment belongs to a user
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * A comment belongs to a post
+     *
+     * @return BelongsTo
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class, 'post_id');
+    }
 }
